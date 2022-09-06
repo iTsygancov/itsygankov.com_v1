96d4d7f43a2e29ce3eaca59b727b484f99c3fd27
iTsygancov <93081413+iTsygancov@users.noreply.github.com>
2022-09-06 22:43:26 +0300

    fix: fix width attribute

--- components/Icon/_Search/IconSearch.tsx
+++ components/Icon/_Search/IconSearch.tsx
@@ -1,8 +1,8 @@
 const IconSearch = () => {
   return (
     <svg
-      width="auto"
-      height="auto"
+      width="24"
+      height="24"
       viewBox="0 0 16 16"
       xmlns="http://www.w3.org/2000/svg"
       fill="currentColor"
