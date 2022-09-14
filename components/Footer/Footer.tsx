ef86aa4bf42f8afd1d11c5f4b66d36e42a6b9dba
iTsygancov <93081413+iTsygancov@users.noreply.github.com>
2022-09-14 22:17:00 +0300

    feat: add Footer component

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,28 @@
+import { Container } from '@mantine/core';
+
+import Logo from '../Logo/Logo';
+import Navbar from '../Navbar/Navbar';
+
+
+const cssPrefix = 'footer';
+
+const Footer = () => {
+  return (
+    <footer className={cssPrefix}>
+      <Container size='xl'>
+        <div className={`${cssPrefix}__content`}>
+          <div className="footer__menu">
+            <Logo />
+            <Navbar />
+          </div>
+          <hr />
+          <p className={`${cssPrefix}__copyright`}>
+            Copyright © 2022 | Igor Tsygankov
+          </p>
+        </div>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
