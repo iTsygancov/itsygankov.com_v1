7dde5ab4318c037a5a17183d4945715746fe4f4d
iTsygancov <93081413+iTsygancov@users.noreply.github.com>
2022-09-14 22:17:37 +0300

    refactor: add footer to layout

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,3 +1,6 @@
+import { ReactNode } from 'react';
+
+import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 
 
@@ -10,7 +13,8 @@ const Layout = ({ children }: LayoutProps) => {
   return (
     <>
       <Header />
-      <main>{children}</main>
+      <main className='content'>{children}</main>
+      <Footer />
     </>
   );
 };
