3870e69897db9b6fe1ad0b2496055f76de8eadea
iTsygancov <93081413+iTsygancov@users.noreply.github.com>
2022-10-30 02:07:56 +0300

    feat: add framer-motion

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,3 +1,5 @@
+import { AnimatePresence, motion } from 'framer-motion';
+import { useRouter } from 'next/router';
 import { ReactNode } from 'react';
 
 import Footer from '../Footer/Footer';
@@ -10,10 +12,21 @@ type LayoutProps = {
 
 
 const Layout = ({ children }: LayoutProps) => {
+  const router = useRouter();
   return (
     <>
       <Header />
-      <main className='content'>{children}</main>
+      <AnimatePresence initial={false} exitBeforeEnter>
+        <motion.main
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          className='content'
+          key={router.route}
+        >
+          {children}
+        </motion.main>
+      </AnimatePresence>
       <Footer />
     </>
   );
