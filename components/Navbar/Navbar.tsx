77daf918c0a20ca87501484c9f23a417c62b3caa
iTsygancov <93081413+iTsygancov@users.noreply.github.com>
2022-10-31 20:10:41 +0300

    fix: refactor Navbar component

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -14,17 +14,35 @@ const Navbar = ({ style, handleClick }: NavbarProps) => {
   return (
     <nav className={cssPrefix} style={{ ...style }}>
       <ul className={`${cssPrefix}__menu`}>
-        <li className={router.pathname === '/posts' ? `${cssPrefix}__menu-item ${cssPrefix}__menu-item--active` : `${cssPrefix}__menu-item` }>
+        <li
+          className={
+            router.pathname === '/posts'
+              ? `${cssPrefix}__menu-item ${cssPrefix}__menu-item--active` 
+              : `${cssPrefix}__menu-item`
+          }
+        >
           <Link href='/posts'> 
             <a className={`${cssPrefix}__menu-link`} onClick={handleClick}>Posts</a>
           </Link>
         </li>
-        <li className={router.pathname === '/about' ? `${cssPrefix}__menu-item ${cssPrefix}__menu-item--active` : `${cssPrefix}__menu-item` }>
+        <li
+          className={
+            router.pathname === '/about'
+              ? `${cssPrefix}__menu-item ${cssPrefix}__menu-item--active` 
+              : `${cssPrefix}__menu-item`
+          }
+        >
           <Link href='/about'>
             <a className={`${cssPrefix}__menu-link`} onClick={handleClick}>About me</a>
           </Link>
         </li>
-        <li className={router.pathname === '/contacts' ? `${cssPrefix}__menu-item ${cssPrefix}__menu-item--active` : `${cssPrefix}__menu-item` }>
+        <li
+          className={
+            router.pathname === '/contacts'
+              ? `${cssPrefix}__menu-item ${cssPrefix}__menu-item--active` 
+              : `${cssPrefix}__menu-item`
+          }
+        >
           <Link href='/contacts'>
             <a className={`${cssPrefix}__menu-link`} onClick={handleClick}>Contacts</a>
           </Link>
