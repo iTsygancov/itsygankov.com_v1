7cd522dae22ccfd77d599cc3fadaa3ac2ad488df
iTsygancov <93081413+iTsygancov@users.noreply.github.com>
2022-11-06 01:06:37 +0300

    feat: add Hero component locales

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,27 +1,29 @@
 import { Container } from '@mantine/core';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 import hero from '../../public/assets/hero.svg';
+import { en, ru } from './Hero.locales';
 
 
 const cssPrefix = 'hero';
 
 
 const Hero = () => {
+  const router = useRouter();
+  const currentLocale = router.locale === 'en' ? en : ru;
   return (
     <Container size='xl'>
       <div className="hero">
         <div className={`${cssPrefix}__content`}>
           <h1 className={`${cssPrefix}__title`}>
-            Hey, I&apos;m Igor!
+            {currentLocale.title}
           </h1>
           <p className={`${cssPrefix}__subtitle`}>
-            Welcome to my website.
+            {currentLocale.subtitle}
           </p>
           <p className={`${cssPrefix}__subtitle`}>
-            Here I share my experience as a
-            Frontend Developer and everything I learn about Javascript,
-            React, Typescript, and much more.
+            {currentLocale.description}
           </p>
         </div>
         <div className={`${cssPrefix}__image`}>
