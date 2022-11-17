97822b61dd9527c7854715fba55a5ac07150232a
iTsygancov <93081413+iTsygancov@users.noreply.github.com>
2022-11-17 23:40:19 +0300

    feat: add locale and refactor Navbar component

---
+++ components/Navbar/Navbar.locale.js
@@ -0,0 +1,13 @@
+const en = {
+  posts: 'Posts',
+  aboutMe: 'About me',
+  contacts: 'Contacts',
+};
+
+const ru = {
+  posts: 'Статьи',
+  aboutMe: 'Обо мне',
+  contacts: 'Контакты',
+};
+
+export { en, ru };
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,52 +1,48 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
+import { en, ru } from './Navbar.locale';
+
 
 type NavbarProps = {
-  style?: object,
-  handleClick?: () => void
-}
+  style?: object;
+  handleClick?: () => void;
+};
 
 const cssPrefix = 'navbar';
 
 const Navbar = ({ style, handleClick }: NavbarProps) => {
   const router = useRouter();
+  const currentLocale = router.locale === 'en' ? en : ru;
+  const links = [
+    { text: currentLocale.posts, link: '/posts' },
+    { text: currentLocale.aboutMe, link: '/about' },
+    { text: currentLocale.contacts, link: '/contacts' },
+  ];
+
   return (
     <nav className={cssPrefix} style={{ ...style }}>
       <ul className={`${cssPrefix}__menu`}>
-        <li
-          className={
-            router.pathname === '/posts'
-              ? `${cssPrefix}__menu-item ${cssPrefix}__menu-item--active` 
-              : `${cssPrefix}__menu-item`
-          }
-        >
-          <Link href='/posts'> 
-            <a className={`${cssPrefix}__menu-link`} onClick={handleClick}>Posts</a>
-          </Link>
-        </li>
-        <li
-          className={
-            router.pathname === '/about'
-              ? `${cssPrefix}__menu-item ${cssPrefix}__menu-item--active` 
-              : `${cssPrefix}__menu-item`
-          }
-        >
-          <Link href='/about'>
-            <a className={`${cssPrefix}__menu-link`} onClick={handleClick}>About me</a>
-          </Link>
-        </li>
-        <li
-          className={
-            router.pathname === '/contacts'
-              ? `${cssPrefix}__menu-item ${cssPrefix}__menu-item--active` 
-              : `${cssPrefix}__menu-item`
-          }
-        >
-          <Link href='/contacts'>
-            <a className={`${cssPrefix}__menu-link`} onClick={handleClick}>Contacts</a>
-          </Link>
-        </li>
+        {links.map((el) => {
+          const { text, link } = el;
+          return (
+            <li
+              className={
+                router.pathname === link
+                  ? `${cssPrefix}__menu-item ${cssPrefix}__menu-item--active`
+                  : `${cssPrefix}__menu-item`
+              }
+              key={link}
+            >
+              <Link href={link}>
+                <a className={`${cssPrefix}__menu-link`} onClick={handleClick}>
+                  {text}
+                </a>
+              </Link>
+            </li>
+          );
+        },
+        )}
       </ul>
     </nav>
   );
