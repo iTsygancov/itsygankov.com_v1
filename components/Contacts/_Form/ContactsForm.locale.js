88c1bcdc2977ac7277bc442e476318341e807cd2
iTsygancov <93081413+iTsygancov@users.noreply.github.com>
2022-11-18 01:29:17 +0300

    feat: update ContactsForm locale

--- components/Contacts/_Form/ContactsForm.locale.js
+++ components/Contacts/_Form/ContactsForm.locale.js
@@ -4,6 +4,10 @@ const en = {
   title: 'Title',
   message: 'Message',
   button: 'Send',
+  modal: {
+    title: 'Thank you! Your letter has been sent!',
+    text: 'I received your letter, thank you for your time. If your letter needs a response, I will definitely try to get back to you as soon as possible.  ',
+  },
 };
 
 const ru = {
@@ -12,6 +16,10 @@ const ru = {
   title: 'Тема',
   message: 'Сообщение',
   button: 'Отправить',
+  modal: {
+    title: 'Спасибо! Ваше письмо отправлено!',
+    text: 'Я получил Ваше письмо, спасибо за уделённое время. Если письмо требует ответа - я обязательно постараюсь связаться с Вами как можно быстрее.',
+  },
 };
 
 export { en, ru };
