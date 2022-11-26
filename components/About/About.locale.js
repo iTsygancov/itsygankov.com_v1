f28dad422632b3b3c6893210a544c63679868c2e
iTsygancov <93081413+iTsygancov@users.noreply.github.com>
2022-11-26 23:44:06 +0300

    fix: fix "About me" locales

--- components/About/About.locale.js
+++ components/About/About.locale.js
@@ -2,7 +2,7 @@ const en = {
   title: 'About me',
   descriptionText: [
     'I\'m a Front-End developer living in Moscow. \n I\'m seriously into creating intuitive, dynamic user interfaces, \n web design and animation.',
-    'Well-organized person, problem solver, worker with high attention \n to detail. Lover of outdoor activities, soap operas and music. \n Family man and father of one fidgety daughter. Interested in the \n whole spectrum of frontend and working on ambitious projects \n with positive people.',
+    'Well-organized person, problem solver, worker with high attention \n to detail. Lover of outdoor activities, soap operas and music. \n Family man and father of one fidgety girl. Interested in the \n whole spectrum of frontend and working on ambitious projects \n with positive people.',
   ],
   outro: 'Do you want to chat? You can always ',
   link: 'contact me',
@@ -12,7 +12,7 @@ const ru = {
   title: 'Обо мне',
   descriptionText: [
     'Я Front-End разработчик, живущий в Москве. Серьезно увлекаюсь созданием интуитивно понятного, динамичного пользовательского интерфейса, веб дизайном и анимацией.',
-    'Хорошо организованный человек, решающий проблемы, работник с высоким вниманием к деталям. Любитель активного отдыха, сериалов и музыки. Семейный человек и отец одной непоседливой дочки. Интересуюсь всем спектром фронтенда и работой над амбициозными проектами с позитивными людьми.',
+    'Хорошо организованный человек, решающий проблемы, работник с высоким вниманием к деталям. Любитель активного отдыха, сериалов и музыки. Семейный человек и отец одной непоседливой девочки. Интересуюсь всем спектром фронтенда и работой над амбициозными проектами с позитивными людьми.',
   ],
   outro: 'Хотите пообщаться? Вы всегда можете ',
   link: 'связаться со мной',
