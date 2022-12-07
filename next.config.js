39ba4d88fe344a30386b0a42b907dc3d6bb41458
iTsygancov <93081413+iTsygancov@users.noreply.github.com>
2022-12-07 16:15:06 +0300

    fix: set localeDetection

--- next.config.js
+++ next.config.js
@@ -2,8 +2,5 @@ module.exports = {
   i18n: {
     locales: ['en', 'ru'],
     defaultLocale: 'en',
-
-    // TODO: Remove localeDetection after finishing with localisation work§
-    localeDetection: false,
   },
 };
