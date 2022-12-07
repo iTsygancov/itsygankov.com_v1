210f5a068f092b97f0ea2c674820cc37c78900de
iTsygancov <93081413+iTsygancov@users.noreply.github.com>
2022-12-07 19:11:39 +0300

    feat: add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/main.sass';
 
 import { MantineProvider } from '@mantine/core';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 import Layout from '../components/Layout/Layout';
 
@@ -32,6 +33,26 @@ function MyApp({ Component, pageProps }: AppProps) {
         },
       }}
     >
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/assets/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/assets/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/assets/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/public/site.webmanifest" />
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
