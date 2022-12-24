0bd323c603915f38b4aa1cecff2a01301fcdf9b2
iTsygancov <93081413+iTsygancov@users.noreply.github.com>
2022-12-25 00:39:40 +0300

    feat: add 'Blog' page meta tags

--- components/Blog/Blog.locale.js
+++ components/Blog/Blog.locale.js
@@ -1,5 +1,19 @@
-const en = { title: 'Blog' };
+const en = {
+  title: 'Blog',
+  description: 'Here I share my experience as a Frontend Developer and everything I learn about Javascript, React, Typescript, and much more.', 
+  meta: {
+    title: 'Blog | < iTsygankov />',
+    description: 'Articles, tutorials and more',
+  },
+};
 
-const ru = { title: 'Блог' };
+const ru = {
+  title: 'Блог',
+  description: 'Здесь я делюсь своим опытом работы в качестве фронтенд разработчика и всем, что я узнаю о Javascript, React, Typescript, а также многом другом.', 
+  meta: {
+    title: 'Блог | < iTsygankov />',
+    description: 'Статьи, обучающие руководства и многое другое',
+  },
+};
 
 export { en, ru };
--- components/Blog/Blog.tsx
+++ components/Blog/Blog.tsx
@@ -1,4 +1,5 @@
 import { Container, Grid } from '@mantine/core';
+import Head from 'next/head';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
@@ -44,29 +45,39 @@ const Posts = ({ posts }: PostsProps) => {
   }, []);
 
   return (
-    <Container size="xl" className={cssPrefix}>
-      <Grid>
-        <Grid.Col lg={9}>
-          <h1 className={`${cssPrefix}__title`}>
-            {query.category || currentLocale.title}
-          </h1>
-          <div className={`${cssPrefix}__list`}>
-            {router.locale === 'en' && Object.keys(query).length !== 0
-              ? enPosts.map(
-                (post) =>
-                  post.data.category === query.category && (
-                    <Link
-                      as={`/blog/${post.filePath.replace(/\.mdx?$/, '')}`}
-                      href={'/blog/[slug]'}
-                      key={post.data.id}
-                    >
-                      <a className={`${cssPrefix}__link`}>
-                        {post.data.title}
-                      </a>
-                    </Link>
-                  ),
-              )
-              : router.locale === 'en' &&
+    <>
+      <Head>
+        <title>{currentLocale.meta.title}</title>
+        <meta
+          name="description"
+          content={currentLocale.meta.description}
+        />
+        <meta property="og:title" content={currentLocale.meta.title} />
+        <meta property="og:description" content={currentLocale.meta.description} />
+      </Head>
+      <Container size="xl" className={cssPrefix}>
+        <Grid>
+          <Grid.Col md={9}>
+            <h1 className={`${cssPrefix}__title`}>
+              {query.category || currentLocale.title}
+            </h1>
+            <div className={`${cssPrefix}__list`}>
+              {router.locale === 'en' && Object.keys(query).length !== 0
+                ? enPosts.map(
+                  (post) =>
+                    post.data.category === query.category && (
+                      <Link
+                        as={`/blog/${post.filePath.replace(/\.mdx?$/, '')}`}
+                        href={'/blog/[slug]'}
+                        key={post.data.id}
+                      >
+                        <a className={`${cssPrefix}__link`}>
+                          {post.data.title}
+                        </a>
+                      </Link>
+                    ),
+                )
+                : router.locale === 'en' &&
                 enPosts.map((post) => (
                   <Link
                     as={`/blog/${post.filePath.replace(/\.mdx?$/, '')}`}
@@ -77,22 +88,22 @@ const Posts = ({ posts }: PostsProps) => {
                   </Link>
                 ))}
 
-            {router.locale === 'ru' && Object.keys(query).length !== 0
-              ? ruPosts.map(
-                (post) =>
-                  post.data.category === query.category && (
-                    <Link
-                      as={`/blog/${post.filePath.replace(/\.mdx?$/, '')}`}
-                      href={'/blog/[slug]'}
-                      key={post.data.id}
-                    >
-                      <a className={`${cssPrefix}__link`}>
-                        {post.data.title}
-                      </a>
-                    </Link>
-                  ),
-              )
-              : router.locale === 'ru' &&
+              {router.locale === 'ru' && Object.keys(query).length !== 0
+                ? ruPosts.map(
+                  (post) =>
+                    post.data.category === query.category && (
+                      <Link
+                        as={`/blog/${post.filePath.replace(/\.mdx?$/, '')}`}
+                        href={'/blog/[slug]'}
+                        key={post.data.id}
+                      >
+                        <a className={`${cssPrefix}__link`}>
+                          {post.data.title}
+                        </a>
+                      </Link>
+                    ),
+                )
+                : router.locale === 'ru' &&
                 ruPosts.map((post) => (
                   <Link
                     as={`/blog/${post.filePath.replace(/\.mdx?$/, '')}`}
@@ -102,17 +113,19 @@ const Posts = ({ posts }: PostsProps) => {
                     <a className={`${cssPrefix}__link`}>{post.data.title}</a>
                   </Link>
                 ))}
-          </div>
-        </Grid.Col>
-        <Grid.Col lg={3}>
-          {
-            router.locale === 'en'
-              ? <PostsCategories categories={enCategories} />
-              : <PostsCategories categories={ruCategories} />
-          }
-        </Grid.Col>
-      </Grid>
-    </Container>
+            </div>
+          </Grid.Col>
+          <Grid.Col md={3}>
+            {
+              router.locale === 'en'
+                ? <PostsCategories categories={enCategories} />
+                : <PostsCategories categories={ruCategories} />
+            }
+          </Grid.Col>
+        </Grid>
+      </Container>
+
+    </>
   );
 };
 
