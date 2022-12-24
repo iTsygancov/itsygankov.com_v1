9c3a3ddee0d98cb2c798c061e42272d2316cec31
iTsygancov <93081413+iTsygancov@users.noreply.github.com>
2022-12-25 00:40:17 +0300

    feat: add 'Contacts' page meta tags

--- components/Contacts/Contacts.locale.js
+++ components/Contacts/Contacts.locale.js
@@ -2,12 +2,20 @@ const en = {
   title: 'Contacts',
   descriptionTitle: 'How can I help?',
   descriptionText: 'Fill out the form or contact me:',
+  meta: {
+    title: 'Contacts | < iTsygankov />',
+    description: 'Fill out the form or contact me.',
+  },
 };
 
 const ru = {
   title: 'Контакты',
   descriptionTitle: 'Чем я могу помочь?',
   descriptionText: 'Заполните форму или свяжитесь со мной:',
+  meta: {
+    title: 'Контакты | < iTsygankov />',
+    description: 'Заполните форму или свяжитесь со мной.',
+  },
 };
 
 export { en, ru };
--- components/Contacts/Contacts.tsx
+++ components/Contacts/Contacts.tsx
@@ -1,4 +1,5 @@
 import { Container, Grid } from '@mantine/core';
+import Head from 'next/head';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 
@@ -14,70 +15,82 @@ const Contacts = () => {
   const router = useRouter();
   const currentLocale = router.locale === 'en' ? en : ru;
   return (
-    <Container size="xl">
-      <div className={cssPrefix}>
-        <h1 className={`${cssPrefix}__title`}>{currentLocale.title}</h1>
-        <Grid gutter="xl">
-          <Grid.Col lg={4} md={5}>
-            <div className={`${cssPrefix}__description`}>
-              <h2 className={`${cssPrefix}__description-title`}>
-                {currentLocale.descriptionTitle}
-              </h2>
-              <p className={`${cssPrefix}__description-text`}>
-                {currentLocale.descriptionText}
-              </p>
-            </div>
-            <div className={`${cssPrefix}__links`}>
-              <div className={`${cssPrefix}__item`}>
-                <div className={`${cssPrefix}__item-icon`}>
-                  <Image
-                    src={message}
-                    layout='intrinsic'
-                    width={32}
-                    height={32}
-                    alt="Message icon"
-                  />
-                </div>
-                <div className={`${cssPrefix}__item-content`}>
-                  <span className={`${cssPrefix}__item-label`}>Email</span>
-                  <a
-                    className={`${cssPrefix}__item-link`}
-                    href="mailto:info@itsygankov.com"
-                    target="blank"
-                  >
+    <>
+      <Head>
+        <title>{currentLocale.meta.title}</title>
+        <meta
+          name="description"
+          content={currentLocale.meta.description}
+        />
+        <meta property="og:title" content={currentLocale.meta.title} />
+        <meta property="og:description" content={currentLocale.meta.description} />
+      </Head>
+      <Container size="xl">
+        <div className={cssPrefix}>
+          <h1 className={`${cssPrefix}__title`}>{currentLocale.title}</h1>
+          <Grid gutter="xl">
+            <Grid.Col lg={4} md={5}>
+              <div className={`${cssPrefix}__description`}>
+                <h2 className={`${cssPrefix}__description-title`}>
+                  {currentLocale.descriptionTitle}
+                </h2>
+                <p className={`${cssPrefix}__description-text`}>
+                  {currentLocale.descriptionText}
+                </p>
+              </div>
+              <div className={`${cssPrefix}__links`}>
+                <div className={`${cssPrefix}__item`}>
+                  <div className={`${cssPrefix}__item-icon`}>
+                    <Image
+                      src={message}
+                      layout='intrinsic'
+                      width={32}
+                      height={32}
+                      alt="Message icon"
+                    />
+                  </div>
+                  <div className={`${cssPrefix}__item-content`}>
+                    <span className={`${cssPrefix}__item-label`}>Email</span>
+                    <a
+                      className={`${cssPrefix}__item-link`}
+                      href="mailto:info@itsygankov.com"
+                      target="blank"
+                    >
                   info@itsygankov.com
-                  </a>
+                    </a>
+                  </div>
                 </div>
-              </div>
-              <div className={`${cssPrefix}__item`}>
-                <div className={`${cssPrefix}__item-icon`}>
-                  <Image
-                    src={telegram}
-                    width={32}
-                    height={32}
-                    layout="intrinsic"
-                    alt="Telegram icon"
-                  />
-                </div>
-                <div className={`${cssPrefix}__item-content`}>
-                  <span className={`${cssPrefix}__item-label`}>Telegram</span>
-                  <a
-                    className={`${cssPrefix}__item-link`}
-                    href="https://t.me/I_Tsygankov"
-                    target="blank"
-                  >
+                <div className={`${cssPrefix}__item`}>
+                  <div className={`${cssPrefix}__item-icon`}>
+                    <Image
+                      src={telegram}
+                      width={32}
+                      height={32}
+                      layout="intrinsic"
+                      alt="Telegram icon"
+                    />
+                  </div>
+                  <div className={`${cssPrefix}__item-content`}>
+                    <span className={`${cssPrefix}__item-label`}>Telegram</span>
+                    <a
+                      className={`${cssPrefix}__item-link`}
+                      href="https://t.me/I_Tsygankov"
+                      target="blank"
+                    >
                   @I_Tsygankov
-                  </a>
+                    </a>
+                  </div>
                 </div>
               </div>
-            </div>
-          </Grid.Col>
-          <Grid.Col lg={8} md={7}>
-            <ContactsForm />
-          </Grid.Col>
-        </Grid>
-      </div>
-    </Container>
+            </Grid.Col>
+            <Grid.Col lg={8} md={7}>
+              <ContactsForm />
+            </Grid.Col>
+          </Grid>
+        </div>
+      </Container>
+
+    </>
   );
 };
 
